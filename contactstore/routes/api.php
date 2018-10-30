753e3b3483de7b9fe9f2707fa219d775c480faf1
Matt Fewer <mattfewer@hotmail.com>
2018-10-30 12:21:39 +0100

    started vue proj

---
+++ contactstore/routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Contact;
+
+Route::group(['middleware' => 'api'], function() {
+    // Fetch contacts
+    Route::get('contact', function() {
+        return Contact::latest()->orderBy('create_at', 'desc')->get();
+    });
+
+    // Get single contact
+    Route::get('contact/{id}', function($id) {
+        return Contact::findOrFail($id);
+    });
+
+    // Add contact
+    Route::post('contact/store', function(Request $request) {
+        return Contact::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone')
+        ]);
+    });
+
+    // Update contact
+    Route::patch('contact/{id}', function(Request $request, $id) {
+        Contact::findOrFail($id)->update([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone')
+        ]);
+    });
+
+    // Delete contact
+    Route::delete('contact/{id}', function($id) {
+        return Contact::destroy($id);
+    });
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
